e2df45557fd313f6222f7cab4cfafd63f87d7370
eros <george.marr.cobc@gmail.com>
2018-08-31 15:20:06 +0100

    adding this shit

---
+++ IPAddress.cpp
@@ -0,0 +1,15 @@
+//
+// Created by george on 31/08/18.
+//
+
+typedef std::vector <IPAdress> List
+
+enum
+{
+    MAX_LENGTH = sizeof(struct in_addr)
+};
+
+//contains basic information regarding an ip address, will be used later down the line but not right away
+enum family {
+
+};
