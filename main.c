1ba40990c9239e32f7fa2f2513b45a71d01cdabc
eros <george.marr.cobc@gmail.com>
2018-08-31 13:40:42 +0100

    finishes C support

--- main.c
+++ main.c
@@ -8,5 +8,40 @@
 
 int main(int argc, char *argv[]) {
 
+    struct addrinfo hint, *res, p;
+    int error;
+
+    if (argc != 2){
+        return 1;
+    }
+
+    memeset(&hint, 0, sizeof(hint));
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_socketype = SOCK_STREAM;
+
+    err = getaddrinfo(argv[1], "http", &hint, &res);
+
+    if(err != 0){
+      printf("Error caught!");
+      return err;
+    }
+
+    void *addr;
+    struct sockaddr_in *ipv4;
+    struct sockaddr_in6  *ipv6;
+
+    for(p = res; p != NULL; p = p -> ai_next){
+        if(p->ai_family == AF_INET){
+            ipv4(struct sockaddr_in *) p-> ai_addr;
+            addr = &(ipv4->sin_addr);
+        } else {
+            ipv6(struct sockaddr_in6 *)p-> ai_addr;
+            addr = &(ipv6->sin6_addr);
+        }
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        printf("%s\n", ipstr);
+    }
+    freeaddrinfo(res);
+
     return 0
 }
