a4252188f6b09d708871a38997b1ed6fe75c2906
eros <george.marr.cobc@gmail.com>
2018-08-31 13:54:55 +0100

    addds in python ipv4 check

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
 import logging
+import lxml.objectify
+import re
+
+
+def is_ipv4(ipv4):
+    re_ipv4 = re.compile('^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[09][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$)')
+
+    if re_ipv4.match(ipv4) is not None:
+        return True
+    return False
+
+
+def normalize_rfc952(name):
+    normalised = str(name).lower().strip('.')
