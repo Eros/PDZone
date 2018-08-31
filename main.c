0ef29fde716260451884713c5bb443f7a9b3b2d7
eros <george.marr.cobc@gmail.com>
2018-08-31 13:34:18 +0100

    init

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0
+}
